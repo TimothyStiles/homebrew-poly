2690ea5e12945d921e6ad8c9fc8838c8ee8e1b7f
Tim <TimothyStiles@users.noreply.github.com>
2020-10-30 12:58:13 -0700

    Brew formula update for poly version v0.4.0

--- Formula/poly.rb
+++ Formula/poly.rb
@@ -2,16 +2,16 @@
 class Poly < Formula
   desc "A command line utility for engineering organisms."
   homepage "https://github.com/TimothyStiles/poly"
-  version "0.3.1"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/timothystiles/poly/releases/download/v0.3.1/poly_0.3.1_Darwin_x86_64.tar.gz"
-    sha256 "2852715a417648feaaf55c4a4b7fabbd93becac789f3dcbf81212d05e98dbc35"
+    url "https://github.com/timothystiles/poly/releases/download/v0.4.0/poly_0.4.0_Darwin_x86_64.tar.gz"
+    sha256 "ba82362cdc9d1e08ec238953efc7906ca339a5d502f27776e7bef3612bb09def"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/timothystiles/poly/releases/download/v0.3.1/poly_0.3.1_Linux_x86_64.tar.gz"
-      sha256 "e7b2a61a7134d9955432afc9e08ddd0e5763230716430abe7b178124703b0c65"
+      url "https://github.com/timothystiles/poly/releases/download/v0.4.0/poly_0.4.0_Linux_x86_64.tar.gz"
+      sha256 "c57a514d16d96e6939dbca60fcbd7bc57ce33b736d8bc7ecdc9fb0fd707a9592"
     end
   end
   
