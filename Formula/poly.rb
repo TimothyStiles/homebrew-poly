64596475d54f9b16a9a2229293a128fb9e3edcf4
Tim <TimothyStiles@users.noreply.github.com>
2021-09-11 16:36:06 -0700

    Brew formula update for poly version v0.13.3

--- Formula/poly.rb
+++ Formula/poly.rb
@@ -5,28 +5,28 @@
 class Poly < Formula
   desc "A command line utility for engineering organisms."
   homepage "https://github.com/TimothyStiles/poly"
-  version "0.13.2"
+  version "0.13.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/timothystiles/poly/releases/download/v0.13.2/poly_0.13.2_Darwin_x86_64.tar.gz"
-      sha256 "6ee354abe38d280facfaba20acced29c0b6e7649d8d565f882a1d79da6a18546"
+      url "https://github.com/timothystiles/poly/releases/download/v0.13.3/poly_0.13.3_Darwin_x86_64.tar.gz"
+      sha256 "827ae3a99075a2d4f5241a1ddfb6c15762aec97b8d3c034de1011b6232dfa595"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/timothystiles/poly/releases/download/v0.13.2/poly_0.13.2_Darwin_arm64.tar.gz"
-      sha256 "84fff770e2ad8cbda7f5fe5a78a7a44a6a7659de2406469187980a74d1a42a8c"
+      url "https://github.com/timothystiles/poly/releases/download/v0.13.3/poly_0.13.3_Darwin_arm64.tar.gz"
+      sha256 "8ac38d494fa56f2e826885cb44be71539ba8f114fb253af8e842119ec06a948e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/timothystiles/poly/releases/download/v0.13.2/poly_0.13.2_Linux_x86_64.tar.gz"
-      sha256 "1b278569ab571b1d97b5a1a8500f90cf13fad685c58516ea131a5267f860195c"
+      url "https://github.com/timothystiles/poly/releases/download/v0.13.3/poly_0.13.3_Linux_x86_64.tar.gz"
+      sha256 "86c9822282d6c6f4aa7fbef5971c03d03191747c9363359385ec9af3f46a9ccb"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/timothystiles/poly/releases/download/v0.13.2/poly_0.13.2_Linux_arm64.tar.gz"
-      sha256 "e7bc52e5aaf338a38e4d92342c83dde2db00396748dffe0ee56df6e2defc2a4f"
+      url "https://github.com/timothystiles/poly/releases/download/v0.13.3/poly_0.13.3_Linux_arm64.tar.gz"
+      sha256 "e85b3385639273640c634b96c1b208afea1f257088f35aea3db0513de1099336"
     end
   end
 
