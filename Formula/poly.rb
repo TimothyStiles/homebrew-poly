cee64c625fd90572b1e6afaff7a0a29204b0ff7b
Tim <TimothyStiles@users.noreply.github.com>
2021-11-13 09:45:06 -0800

    Brew formula update for poly version v0.16.0

--- Formula/poly.rb
+++ Formula/poly.rb
@@ -5,36 +5,47 @@
 class Poly < Formula
   desc "A command line utility for engineering organisms."
   homepage "https://github.com/TimothyStiles/poly"
-  version "0.15.0"
-  bottle :unneeded
+  version "0.16.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/timothystiles/poly/releases/download/v0.15.0/poly_0.15.0_Darwin_arm64.tar.gz"
-      sha256 "9499e3c11c02344375092544d3fe8a28679ac9e3a021f5d8027993a701884831"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/timothystiles/poly/releases/download/v0.15.0/poly_0.15.0_Darwin_x86_64.tar.gz"
-      sha256 "bfa0f35afd3fe02808dc62bcfd0f73c171daeb3fdd4efe76b528dd9a087aea6a"
+      url "https://github.com/timothystiles/poly/releases/download/v0.16.0/poly_0.16.0_Darwin_x86_64.tar.gz"
+      sha256 "c269b18f855c8666818843c42545a7fbdfedbf2fa4cd5cf0cff4ce9e97c63563"
+
+      def install
+        bin.install "poly"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/timothystiles/poly/releases/download/v0.16.0/poly_0.16.0_Darwin_arm64.tar.gz"
+      sha256 "5814d9e81e1a9f19b4e4a8696ce5bc989b67df03fba11d328ff3411d02b39717"
+
+      def install
+        bin.install "poly"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/timothystiles/poly/releases/download/v0.15.0/poly_0.15.0_Linux_arm64.tar.gz"
-      sha256 "7de0a3e5f828fb4e5aedafc377bec6239974435d0e673d0c892f06b6adf33500"
+      url "https://github.com/timothystiles/poly/releases/download/v0.16.0/poly_0.16.0_Linux_arm64.tar.gz"
+      sha256 "b39d46cebf9f5b286e73b233cc840516a94f735285614df0f82fae1ca1599186"
+
+      def install
+        bin.install "poly"
+      end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/timothystiles/poly/releases/download/v0.15.0/poly_0.15.0_Linux_x86_64.tar.gz"
-      sha256 "23e09db91dd89c92f2ed6d719989c7ed8d7ba341ac841bdd44625e94fa50c536"
+      url "https://github.com/timothystiles/poly/releases/download/v0.16.0/poly_0.16.0_Linux_x86_64.tar.gz"
+      sha256 "74be34bfaec7af79b3873e8160fcd1753b36a17e5ca53d272eb949eb9cc36654"
+
+      def install
+        bin.install "poly"
+      end
     end
   end
 
   depends_on "go"
-
-  def install
-    bin.install "poly"
-  end
 
   test do
     system "#{bin}/poly -h"
